bc5bab2632fe5d6c1b54919f212faea0ac5a83ff
RafaellSouzaPinto <rafaelsouza.rs14@gmail.com>
2025-03-08 18:10:30 -0300

    Adiciona exercício 5: Type Alias RespostaServidor e função para processar resposta

---
+++ aula-typescript/dist/exercicio5.js
@@ -0,0 +1,12 @@
+function processarResposta(resposta) {
+    if (typeof resposta === 'string') {
+        console.log(`A resposta do servidor é uma string: ${resposta}`);
+    }
+    else if (typeof resposta === 'boolean') {
+        console.log(`A resposta do servidor é um booleano: ${resposta}`);
+    }
+}
+processarResposta("Erro 404: Página não encontrada");
+processarResposta(true);
+processarResposta(false);
+processarResposta("Sucesso: Operação concluída");
